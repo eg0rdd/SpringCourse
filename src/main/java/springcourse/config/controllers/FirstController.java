59491710946e4a78760c5891deb27e456ea13065
Egor <egor.doda5@gmail.com>
2024-03-12 22:10:12 +0300

    commit

---
+++ src/main/java/springcourse/config/controllers/FirstController.java
@@ -0,0 +1,56 @@
+package springcourse.config.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
+
+        return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) double b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        double result;
+        switch (action) {
+
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / b;
+                break;
+            case "substraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+
+        }
+
+        return "first/hello";
+
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+
+}
